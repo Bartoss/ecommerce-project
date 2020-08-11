a2deeaaa764ebdd0e9ce95a9aafd106f71a22428
Bartolomeo <sarata.bartlomiej@student.pk.edu.pl>
2020-08-11 16:38:06 +0200

    jpa entities
    develop jpa entities

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/bartolomeo/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,28 @@
+package com.bartolomeo.ecommerce.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "product_category")
+@Data
+// @Data --know bug
+@Getter
+@Setter
+public class ProductCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> products;
+
+
+}
